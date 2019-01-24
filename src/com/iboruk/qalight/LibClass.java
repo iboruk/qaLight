19dd79f7d280ad81c5e7b490d967acc6863084e9
iboruk <igor.boruk@gmail.com>
2019-01-24 14:19:52 +0200

    init Commit

--- src/com/iboruk/qalight/LibClass.java
+++ src/com/iboruk/qalight/LibClass.java
@@ -6,37 +6,64 @@ package com.iboruk.qalight;
 public class LibClass {
 
     public static void elementsOfArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 10) {
-                System.out.println(i + "-й элемент, равный " + array[i] + " - меньше десяnи");
-            } else if (array[i] == 10) {
-                System.out.println(i + "-й элемент, равный " + array[i] + " - равен десяnи");
-            } else if (array[i] > 10) {
-                System.out.println(i + "-й элемент, равный " + array[i] + " - больше десяnи");
+        for (int a : array) {
+            if (a < 10) {
+                System.out.println(a + " Элемент меньше десяти");
+            } else if (a == 10) {
+                System.out.println(a + " Элемент равен десяти");
+            } else {
+                System.out.println(a + " Элемент больше десяти");
             }
         }
     }
 
-    public static int getSum(int a, int b) {
-        int c = a + b;
-        System.out.println("Сумма равна = " + c);
-        return c;
+    public static int sum(int a, int b) {
+        int rez_1 = a + b;
+        System.out.println("rez_1 = " + rez_1);
+        return rez_1;
     }
 
-    public static String getSum(String a, String b) {
-        String c = a + b;
-        System.out.println("Сумма равна = " + c);
-        return c;
+    public static String sum(String a, String b) {
+        String rez_2 = a + b;
+        System.out.println("rez_2 = " + rez_2);
+        return rez_2;
     }
 
-    public static int getSum(int a, String b) {
-        int c = 0;
+    public static int sum(int a, String b) {
+        int rez_3 = 0;
         try {
+            if (b == null ) {
+                System.out.println("99999");
+                return rez_3;
+            }
             int cc = Integer.valueOf(b);
-            c = a + cc;
-        } catch (RuntimeException e){
-            System.out.println("Строка должна быть только из чисел!");
+            rez_3 = a + cc;
+            System.out.println("rez_3 = " + rez_3);
+        } catch (NumberFormatException e) {
+            System.out.println("88888");
         }
-        return c;
+        return rez_3;
+    }
+
+    public static int multiplication(int a, int b) {
+        int rez_3 = a * b;
+        System.out.println("rez_3 = " + rez_3);
+        return rez_3;
+    }
+
+    public static int division(int a, int b) {
+        int rez_4 = 0;
+        try {
+//            int cc = 0;
+            rez_4 = a / b;
+            System.out.println("rez_4 = " + rez_4);
+        } catch (ArithmeticException e) {
+            if (e.toString().contains("by zero")) {
+                System.out.println("Дfffffff");
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        return rez_4;
     }
 }
--- src/com/iboruk/qalight/MainClass.java
+++ src/com/iboruk/qalight/MainClass.java
@@ -2,12 +2,18 @@ package com.iboruk.qalight;
 
 import com.sun.istack.internal.localization.NullLocalizable;
 
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
         int[] array = new int[]{2, 4, 26, 7, 28, 10, 9, 20, 33};
 
-        //LibClass.elementsOfArray(array);
-        LibClass.getSum(0, 6);
+//        LibClass.elementsOfArray(array);
+//        LibClass.sum(5, 6);
+//        LibClass.multiplication(5, 2);
+
+//       LibClass.sum(2, null);
+       LibClass.division(10, 2);
     }
 
 }
